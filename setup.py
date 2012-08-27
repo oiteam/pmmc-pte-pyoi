033809b7785994abcee9bc967e78501b9a0b930e
Laércio de Sousa <lbsousajr@gmail.com>
2012-08-27 11:12:36 -0300

    Add new script setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='PyPTE',
+      version='4.0.1',
+      description='',
+      author='Laércio de Sousa',
+      author_email='lbsousajr@gmail.com',
+      url='',
+      packages=['pypte'],
+      scripts=['pypai']
+)
