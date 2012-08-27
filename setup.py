05f6fbc8bd73ccbbd3290a6d4afb00d3df3ed001
Laércio de Sousa <lbsousajr@gmail.com>
2012-08-27 11:15:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@
 from distutils.core import setup
 
 setup(name='PyPTE',
-      version='4.0.1',
-      description='',
+      version='4.0.1.0',
+      description='Enhanced launchers/clients for PTE-PMMC projects',
       author='Laércio de Sousa',
       author_email='lbsousajr@gmail.com',
-      url='',
+      url='https://github.com/oiteam/pmmc-pte-pypte.git',
       packages=['pypte'],
       scripts=['pypai']
 )
