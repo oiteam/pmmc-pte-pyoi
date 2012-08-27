eee50092f05d8b2e8fa1691e9c68c9203a1ec298
Laércio de Sousa <lbsousajr@gmail.com>
2012-08-27 16:17:35 -0300

    Small updates in setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from distutils.core import setup
 
 setup(name='PyPTE',
       version='4.0.1.0',
-      description='Enhanced launchers/clients for PTE-PMMC projects',
+      description='Collection of Python utils for PTE-PMMC projects',
       author='Laércio de Sousa',
       author_email='lbsousajr@gmail.com',
+      license='GNU GPLv3'
       url='https://github.com/oiteam/pmmc-pte-pypte.git',
       packages=['pypte'],
-      scripts=['bin/pypai']
-)
+      scripts=['bin/pypai'])
